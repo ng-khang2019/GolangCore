adf002e3393a23bc589f7a6d42bb3e1b6ecad624
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-27 03:13:16 +0700

    Initial commit

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 func main() {
 
 	s := "gopher"
+	var x string = "Khang"
+	fmt.Println("Hi " + x)
 	fmt.Printf("Hello and welcome, %s!\n", s)
 
 	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+		fmt.Println("i = ", 100/i)
 	}
 }
