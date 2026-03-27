37a175f0d3c6c7909d794c2ad32debde1dabf44d
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-27 15:07:33 +0700

    Complete Go function basics

---
+++ _Functions/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Sum of 1, 2 and 3 is ", add(1, 2, 3))
+	fmt.Println()
+
+	a, b := swap("Hello", "World") // Same as var a, b string = "Hello", "World"
+	fmt.Println(swap(a, b))
+
+	fmt.Println(split(add(5, 8, 4)))
+}
+
+/*
+When two or more consecutive named function parameters share a type,
+you can omit the type from all but the last. The example below
+shortened (x int, y int, z int) to (x, y, z int)
+*/
+func add(x, y, z int) int {
+	return x + y + z
+}
+
+/*
+A function can also returns multiple values
+*/
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+/*
+A return statement without arguments returns the named return values.
+*/
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return // This will return the named return values which are x and y
+}
