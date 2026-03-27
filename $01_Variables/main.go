e8f8c48267c3b74a070d5a606dd0e870c38e0dd4
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-27 19:46:15 +0700

    Variables declaration

--- $01_Variables/main.go
+++ $01_Variables/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+)
+
+/*
+In Golang, the "var" statement can declare one or more variables; the var keyword comes first
+then variable name and its type. A var statement can also be at the package or function level
+*/
+
+/*
+IMPORTANT: var and const can be used anywhere in the program, in or outside a function,
+but ":=" can only be used inside a function
+*/
+
+func main() {
+	// Declare without initializing
+	var i int                  // Default value for integer is 0
+	var cpp, python, java bool // boolean variables are false as default
+	var job string             // string default literal is ""
+	fmt.Println("Declare without initializing:")
+	fmt.Println(i, cpp, python, java)
+	fmt.Println(job)
+
+	// Declare and initialization or complete declaration
+	var a string = "Hello"
+	var b, c int = 1, 2
+	fmt.Println(a, b, c)
+
+	// Declare without its type. Go will automatically infer the type
+	var d = "World"
+	var e = 3.14
+	fmt.Println(d)
+	fmt.Println("Pi value is ", e)
+
+	// := operator is used for shorthand declaration. It is commonly used inside a
+	fruit := "Banana"
+	prime1, prime2, prime3 := 2, 3, 5
+	fmt.Println(fruit)
+	fmt.Println(prime1, prime2, prime3)
+	fmt.Println(printApple())
+
+}
+
+func printApple() string {
+	apple := "Apple" // It is similar to var apple string = "Apple"
+	return apple
+}
