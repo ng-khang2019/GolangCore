e9f23f76366c2e2edce1d8f669ef3512b44df0bd
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-05 15:40:10 +0700

    Update runtime package example

--- $00_Packages/main.go
+++ $00_Packages/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -12,6 +13,16 @@ func main() {
 	fmt.Print("My favorite number is ", rand.Intn(10), ". Yolo!\n")
 	fmt.Printf("Current time is: %s\n", time.Now().Local().Format("2006-01-02"))
 	fmt.Printf("Pi constant has the value of %g\n", math.Pi)
-	fmt.Printf("Square root of 7 is %.2f", math.Sqrt(7))
+	fmt.Printf("Square root of 7 is %.2f\n", math.Sqrt(7))
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("macOS.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("Your current OS is %s\n", os)
+	}
 
 }
