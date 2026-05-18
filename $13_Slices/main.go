32345e1bdb6b774ec5f35f755049bf19c06a3db2
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-05-18 18:28:34 +0700

    Add example demonstrating tic-tac-toe board creation and traversal using slices in Go

--- $13_Slices/main.go
+++ $13_Slices/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"slices"
+	"strings"
 )
 
 func main() {
@@ -153,6 +154,25 @@ func main() {
 		}
 	}
 	fmt.Println("2d: ", twoD)
+
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+
 }
 
 func printSlice(s []int) {
