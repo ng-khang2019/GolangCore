c637a3d4c0501461bcdc7cb6a3a7c28334f05399
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-05-18 11:23:28 +0700

    Add example demonstrating pointer declarations, dereferencing, and usage in Go

--- $12_Pointers/main.go
+++ $12_Pointers/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	a, b := 10, 15
+	fmt.Println("Value of a and b: ", a, b)
+
+	// Declaring a pointer
+	var ptr1 *int = &a
+	ptr2 := &b
+	fmt.Println("Pointer value of a and b: ", ptr1, "|", ptr2) // Output reference of the pointers
+
+	// Accessing the value pointed by the pointers
+	fmt.Println("Accessing the value pointed by the pointers:")
+	fmt.Println(*ptr1, *ptr2)
+
+	// De-referencing the pointers
+	*ptr1 = 20 // a = 20 as ptr1 stores the address of a
+	*ptr2 = 30 // b = 30 as ptr2 stores the address of b
+	fmt.Println("De-referencing the pointers:")
+	fmt.Println(a, b)
 
 }
